bf7e8f76c91a0c628fa261c6e985762ad9c6d29c
mpho2024 <mpho.tsatsi01@gmail.com>
2024-10-23 18:00:58 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld // Namespace: A container for classes
+{
+    class Program // Corrected to PascalCase
+    {
+        static void Main(string[] args) // Main method
+        {
+            var x=true;
+            var y=false;
+            var z=false || true;
+        Console.WriteLine(z);
+        }
+    }
+}
